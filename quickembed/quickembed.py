c9152d972dc3e128bbfcc01a6f7a362c3161d90c
Sitryk <jamesfitzy1937@gmail.com>
2017-07-06 07:46:47 +1200

    Update quickembed.py

--- quickembed/quickembed.py
+++ quickembed/quickembed.py
@@ -1,34 +1,37 @@
 from discord.ext import commands
 from __main__ import send_cmd_help
-import os
-import discord
+import random, os, discord
 from .utils.dataIO import dataIO
 
 default_settings = {"default_colour" : "red"}
-
+def returnhex():
+    return random.randint(0, 0xFFFFFF)
 class QEmbed:
 
     def __init__(self, bot):
         self.bot = bot
         self.JSON = "data/Sitryk-Cogs/quickembed/settings.json"
         self.data = dataIO.load_json(self.JSON)
-        self.colours = { "red" : discord.Color.red(),
-                         "dark_red" : discord.Color.dark_red(),
+        self.colours = { "red" : discord.Color.red,
+                         "dark_red" : discord.Color.dark_red,
                          "blue" : discord.Color.blue(),
-                         "dark_blue" : discord.Color.dark_blue(),
-                         "teal" : discord.Color.teal(),
-                         "dark_teal" :discord.Color.dark_teal(),
-                         "green" : discord.Color.green(),
-                         "dark_green" : discord.Color.dark_green(),
-                         "purple" : discord.Color.purple(),
-                         "dark_purple" :discord.Color.dark_purple(),
-                         "magenta" : discord.Color.magenta(),
-                         "dark_magenta" : discord.Color.dark_magenta(),
-                         "gold" :discord.Color.gold(),
-                         "dark_gold" : discord.Color.dark_gold(),
-                         "orange" :discord.Color.orange(),
-                         "dark_orange" :discord.Color.dark_orange()
+                         "dark_blue" : discord.Color.dark_blue,
+                         "teal" : discord.Color.teal,
+                         "dark_teal" :discord.Color.dark_teal,
+                         "green" : discord.Color.green,
+                         "dark_green" : discord.Color.dark_green,
+                         "purple" : discord.Color.purple,
+                         "dark_purple" :discord.Color.dark_purple,
+                         "magenta" : discord.Color.magenta,
+                         "dark_magenta" : discord.Color.dark_magenta,
+                         "gold" :discord.Color.gold,
+                         "dark_gold" : discord.Color.dark_gold,
+                         "orange" :discord.Color.orange,
+                         "dark_orange" :discord.Color.dark_orange,
+                         "random" : returnhex
                          }
+
+
     @commands.group(name="qeset", pass_context=True)
     async def _qeset(self, ctx):
         if ctx.invoked_subcommand is None:
@@ -49,14 +52,14 @@ class QEmbed:
     async def qembed(self, ctx, text, color=None):
         """Used to make a quick embed {0} corresponds to server"""
         if color is None:
-            embed_color = self.colours[self.data["default_colour"]]
+            embed_color = self.colours[self.data["default_colour"]]()
         elif color.lower() not in self.colours:
             if len(color) == 6:
                 try:
                     color = int(color, 16)
                     embed_color = discord.Color(color)
                 except:
-                    embed_color = self.colours[self.data["default_colour"]]
+                    embed_color = self.colours[self.data["default_colour"]]()
             else:
                 msg = "Available colors: \n"
                 for x in self.colours:
@@ -65,7 +68,7 @@ class QEmbed:
                 await self.bot.whisper("```fix\n" + msg + "\n```")
                 return
         else:
-            embed_color = self.colours[color]
+            embed_color = self.colours[color]()
         embed = discord.Embed(description=text.format(ctx.message.server), color=embed_color)
         await self.bot.say(embed=embed)
 
