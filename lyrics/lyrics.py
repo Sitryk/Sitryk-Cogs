631569674fe7790728ba237b6a429dde5a4a9398
Sitryk <jamesfitzy1937@gmail.com>
2017-07-09 17:14:51 +1200

    Selectable from a list

--- lyrics/lyrics.py
+++ lyrics/lyrics.py
@@ -1,6 +1,6 @@
 from discord.ext import commands
 from .utils.chat_formatting import pagify
-import discord, requests
+import discord, requests, random
 try:
   from bs4 import BeautifulSoup
   soupAvailable = True
@@ -11,30 +11,36 @@ class LyricsCog:
 
     def __init__(self, bot):
         self.bot = bot
-        self.song = ""
-        self.artist = ""
+
+    def valid_choice(self, msg):
+      if msg.content.isdigit() and eval(msg.content) in range(0, 11):
+        return True
+      return False
 
     @commands.command(pass_context=True)
-    async def lyrics(self, ctx, artist, song):
+    async def lyrics(self, ctx, *, searchterm: str):
         """Used to fetch lyrics from a song
+            Usage: [p]lyrics humble"""
 
-            Usage: [p]lyric 'kendrick lamar' 'humble' """
-
-        if artist == "":
-            await self.bot.say("Please supply an artist")
-        elif song == "":
-            await self.bot.say("Please supply a song title")
+        searchList = lyricsearch(searchterm)
+        searchText = ""
+        for index, item in enumerate(searchList):
+          searchText += "\n\n**{}.** {}".format(index +1, " - ".join(item)) 
+        chooseList = discord.Embed(description = searchText,color=discord.Color.red())
+        chooseList.set_footer(text="*Type the corresponding number or 0 to cancel*")
+        _sent_in = await self.bot.say(embed=chooseList)
+        choice = await self.bot.wait_for_message(timeout=180, author=ctx.message.author, channel=ctx.message.channel, check=self.valid_choice)
+        if int(choice.content) == 0:
+          await self.bot.say("Cancelling lyric search")
+          return
         else:
-            lyrics = lyricsearch(artist, song)
-            if lyrics == None:
-                await self.bot.whisper("Sorry! I couldn't find any lyrics with your search terms.")
-            else:
-                try:
-                    lyrics = pagify(lyrics)
-                    for page in lyrics:
-                        await self.bot.whisper(page)
-                except discord.DiscordException:
-                    await self.bot.say("I can't send messages to this user.")
+          await self.bot.say("I've sent you the lyrics for **{}**".format(" - ".join(searchList[int(choice.content)-1])))
+          try:
+            lyrics = pagify(lyricsearch(searchterm, int(choice.content)-1))
+            for page in lyrics:
+              await self.bot.whisper(page)
+          except discord.DiscordException:
+            await self.bot.say("I can't send messages to this user.")
         
 
 base_url = "https://api.genius.com"
@@ -55,19 +61,22 @@ def lyrics_from_song_api_path(song_api_path):
   lyrics = html.find("div", class_="lyrics").get_text() #updated css where the lyrics are based in HTML
   return lyrics
 
-def lyricsearch(artist, song):
+def lyricsearch(searchterm, choice=None):
   search_url = base_url + "/search"
-  data = {'q': song}
+  data = {'q': searchterm}
   response = requests.get(search_url, data=data, headers=headers)
   json = response.json()
   song_info = None
+  items = []
+
   for hit in json["response"]["hits"]:
-    if hit["result"]["primary_artist"]["name"].lower() == artist.lower():
-      song_info = hit
-      break
-  if song_info:
-    song_api_path = song_info["result"]["api_path"]
-    return lyrics_from_song_api_path(song_api_path)
+    items.append([hit["result"]["primary_artist"]["name"], hit["result"]["title"]])
+
+  if choice is None:
+    return items
+  else:
+    song_info = json['response']['hits'][choice]['result']['api_path']
+    return lyrics_from_song_api_path(song_info)
 
 def setup(bot):
     if soupAvailable:
